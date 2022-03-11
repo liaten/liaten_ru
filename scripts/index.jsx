a88c7d3ca7115068fc1b70c5cf3d36ba429aa15a
liaten <vlad171vlad@gmail.com>
2022-03-11 21:01:33 +0300

    change

--- scripts/index.jsx
+++ scripts/index.jsx
@@ -33,7 +33,7 @@ for (const props in repos){
     }
     render() {
       let result = this.state.date;
-      result = result.replaceAt(11," ");
+      result = result.replaceAt(10," ");
       return (
         <div>
           <div>{result}</div>
