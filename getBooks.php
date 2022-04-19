791b9fe42f922eb0c59c5e8c3163f998a42cb691
liaten <vlad171vlad@gmail.com>
2022-04-19 23:00:36 +0300

    connection

--- getBooks.php
+++ getBooks.php
@@ -1,28 +1,14 @@
 <?php
-$servername = "server21.hosting.reg.ru";
+$servername = "localhost";
 $username = "u1610989_admin";
-$password = "DFSWG*&$@#^$^&sadasdg212";
+$password = "vF5zW5tT8zxJ1t";
 $dbname = "u1610989_library";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT * FROM 'book'";
- $result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    var_dump($conn->error);
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    // print_r($row);
-    echo "row!"
+if (!$conn) {
+    echo 'Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error();
+    exit;
   }
-} else {
-  echo "0 results";
-}
 $conn->close();
 ?>
