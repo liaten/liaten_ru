13155caa488979b4901c3b8a868684d9cd4fcd41
liaten <vlad171vlad@gmail.com>
2022-04-20 02:26:39 +0300

    change arrays

--- new_7_books.php
+++ new_7_books.php
@@ -7,21 +7,32 @@ $dbname = "u1610989_library";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 $conn->set_charset("utf8");
+
 if (!$conn) {
     echo 'Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error();
     exit;
   }
-  $sql = "SELECT title, author, theme, date FROM `book` order by date desc limit 7;";
-  $result = $conn->query($sql);
+
+$sql = "SELECT title, author, theme, date FROM `book` order by date desc limit 7;";
+$result = $conn->query($sql);
+$response = array();
+
+if(mysqli_num_rows($result)>0){
+  $response['success'] = 1;
   $books = array();
-  if(mysqli_num_rows($result)>0){
-    $books['success'] = 1;
-    while($row = $result->fetch_assoc()) {
-      array_push($books, $row);
-      //$books[] = $row;
-    }
-    $books['books'] = $books;
+  
+  while($row = $result->fetch_assoc()) {
+    array_push($books, $row);
+    //$books[] = $row;
   }
-  echo json_encode($books, JSON_UNESCAPED_UNICODE);
-$conn->close();
+  $response['books'] = $books;
+}
+else{
+  $response['success'] = 0;
+  $response['message'] = 'No data';
+}
+
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
+mysqli_close($conn);
+
 ?>
