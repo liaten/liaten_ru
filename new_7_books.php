462a2da029671d44d37cab274dbad1e24e7df058
liaten <vlad171vlad@gmail.com>
2022-04-20 01:02:27 +0300

    change

--- new_7_books.php
+++ new_7_books.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost";
+$servername = "server21.hosting.reg.ru";
 $username = "u1610989_admin";
 $password = "vF5zW5tT8zxJ1t";
 $dbname = "u1610989_library";
@@ -11,7 +11,7 @@ if (!$conn) {
     echo 'Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error();
     exit;
   }
-  $sql = "SELECT title, author, theme, date FROM `book`;";
+  $sql = "SELECT title, author, theme, date FROM `book` order by date desc limit 7;";
   $result = $conn->query($sql);
   $rows = array();
   while($row = $result->fetch_assoc()) {
