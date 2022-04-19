e0dc4471d19acd0cb3bd44872616cdcbd8cf70d7
liaten <vlad171vlad@gmail.com>
2022-04-19 22:49:52 +0300

    change

--- getBooks.php
+++ getBooks.php
@@ -18,7 +18,8 @@ $result = mysqli_query($conn,$sql)
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    print_r($row);
+    // print_r($row);
+    echo "row!"
   }
 } else {
   echo "0 results";
