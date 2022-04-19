abe1068ef08627f78bf487e5d2c38f860213dc27
liaten <vlad171vlad@gmail.com>
2022-04-19 22:26:50 +0300

    change

--- getBooks.php
+++ getBooks.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "u1610989_admin";
+$password = "DFSWG*&$@#^$^&sadasdg212";
+$dbname = "u1610989_library";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT id FROM book";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "id: " . $row["id"] . "<br>";
   }
 } else {
   echo "0 results";
