9531b7c8ab98e05b5f84ff697aa53ecb72efac28
liaten <vlad171vlad@gmail.com>
2022-04-19 22:28:35 +0300

    title

--- getBooks.php
+++ getBooks.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id FROM book";
+$sql = "SELECT title FROM book;";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"] . "<br>";
+    echo "title: " . $row["title"] . "<br>";
   }
 } else {
   echo "0 results";
