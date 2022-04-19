37f22c54a56d80e49ac4031362a67f5cb1327e75
liaten <vlad171vlad@gmail.com>
2022-04-19 22:49:00 +0300

    change get method

--- getBooks.php
+++ getBooks.php
@@ -11,13 +11,14 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT title FROM book;";
-$result = $conn->query($sql);
+$sql = "SELECT * FROM book";
+// $result = $conn->query($sql);
+$result = mysqli_query($conn,$sql)
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "title: " . $row["title"] . "<br>";
+    print_r($row);
   }
 } else {
   echo "0 results";
