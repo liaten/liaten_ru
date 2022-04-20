4e44e1d1c238b3b941b8885889e8cb4b99bb1297
liaten <vlad171vlad@gmail.com>
2022-04-20 18:25:58 +0300

    userid

--- db/username.php
+++ db/username.php
@@ -13,8 +13,24 @@ $q = mysqli_real_escape_string($conn,$q);
 
 // Save form input
 $user = $_POST['user'];
-mysqli_query($conn,"SELECT * FROM `user` where user like '$user'");
+$sql = "SELECT * FROM `user` where userid like '$user';";
+$result = $conn->query($sql);
+$response = array();
 
-// Close connection
+if(mysqli_num_rows($result)>0){
+  $response['success'] = 1;
+  $books = array();
+  
+  while($row = $result->fetch_assoc()) {
+    array_push($books, $row);
+  }
+  $response['books'] = $books;
+}
+else{
+  $response['success'] = 0;
+  $response['message'] = 'No data';
+}
+
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
 mysqli_close($conn);
 ?>
