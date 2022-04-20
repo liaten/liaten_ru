d4236840b70fa0ca9ffc1710ce8095f4524e310b
liaten <vlad171vlad@gmail.com>
2022-04-20 20:03:53 +0300

    password

---
+++ db/password.php
@@ -0,0 +1,21 @@
+<?php
+   require_once('configuration.php');
+
+   // Create connection
+   $con = new mysqli($servername, $username, $password, $dbname);
+
+   if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
+	
+   $userid = $_GET['userid'];
+   $password = mysqli_query($con,"SELECT password FROM `user` where 
+   userid='$userid'");
+   $row = mysqli_fetch_array($result);
+   $data = $row[0];
+   if($data){
+    echo $data;
+   }
+	
+   mysqli_close($con);
+?>
