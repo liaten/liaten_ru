bfda205174ecd0e21c38f8476430914cf61af74a
liaten <vlad171vlad@gmail.com>
2022-04-20 18:24:04 +0300

    conn

--- db/username.php
+++ db/username.php
@@ -3,18 +3,18 @@
 require_once('configuration.php');
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-if (!$con){
-die('Could not connect: ' . mysqli_error($con));
+if (!$conn){
+die('Could not connect: ' . mysqli_error($conn));
 }
 
-mysqli_select_db($con,$database);
+mysqli_select_db($conn,$database);
 
-$q = mysqli_real_escape_string($con,$q);
+$q = mysqli_real_escape_string($conn,$q);
 
 // Save form input
 $user = $_POST['user'];
-mysqli_query($con,"SELECT * FROM `user` where user like '$user'");
+mysqli_query($conn,"SELECT * FROM `user` where user like '$user'");
 
 // Close connection
-mysqli_close($con);
+mysqli_close($conn);
 ?>
