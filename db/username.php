4bf8db08ce88b9950fe71e876b0f3f8fc729dc3a
liaten <vlad171vlad@gmail.com>
2022-04-20 18:22:32 +0300

    user

--- db/username.php
+++ db/username.php
@@ -1,16 +1,9 @@
 <?php
 $q=$_GET["q"];
-// Load Joomla! configuration file
+// Load configuration file
 require_once('configuration.php');
-// Create a JConfig object
-$config = new JConfig();
-// Get the required codes from the configuration file
-$server = $config->host;
-$username   = $config->user;
-$password   = $config->password;
-$database = $config->db;
 
-$con = mysqli_connect($server,$username,$password,$database);
+$conn = new mysqli($servername, $username, $password, $dbname);
 if (!$con){
 die('Could not connect: ' . mysqli_error($con));
 }
@@ -20,8 +13,8 @@ mysqli_select_db($con,$database);
 $q = mysqli_real_escape_string($con,$q);
 
 // Save form input
-$a10001 = $_POST['a10001'];
-mysqli_query($con,"INSERT INTO cypg8_testtest (a10001) VALUES ('".$a10001."')");
+$user = $_POST['user'];
+mysqli_query($con,"SELECT * FROM `user` where user like '$user'");
 
 // Close connection
 mysqli_close($con);
