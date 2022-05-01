78f1a52e51eb0eebc915e5cfed6cb69055430229
liaten <vlad171vlad@gmail.com>
2022-05-01 23:24:00 +0300

    email

---
+++ db/email.php
@@ -0,0 +1,29 @@
+<?php
+   require_once('configuration.php');
+
+   // Create connection
+   $con = new mysqli($servername, $username, $password, $dbname);
+   $con->set_charset("utf8");
+
+   if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
+	
+   $email = $_GET['email'];
+   $result = mysqli_query($con,"SELECT email FROM `user` where 
+   email='$email'");
+   $row = mysqli_fetch_array($result);
+   $data = $row[0];
+   $response = array();
+   
+   if($data){
+      $response['success']=true;
+      $response['type']='email';
+      $response['email'] = $data;
+   }
+   else{
+      $response['success']=false;
+   }
+   echo json_encode($response, JSON_UNESCAPED_UNICODE);
+   mysqli_close($con);
+?>
